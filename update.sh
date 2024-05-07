1d9a42aa354dbf047690a34294e979b141e566c3
smartwa <smartwacaleb@gmail.com>
2024-05-07 12:02:01 +0300

    feat: Add cron job

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-git checkout master
-git pull origin master
-./start.sh
-git add .
-git commit -m "Update proxies - $(date '+%A %d-%m-%Y %H:%M:%S %Z')"
-git push https://Simatwa:$GITHUB_TOKEN@github.com/Simatwa/free-proxies.git
